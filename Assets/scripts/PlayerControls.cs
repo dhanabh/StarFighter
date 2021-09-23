1de3689701c069365e5e8b915ad7c4ce520597c0
Dhananjay M. Bhate <dhanabh1010@gmail.com>
2021-09-23 18:54:05 +0530

    Added pitch functionality to StarFighter. Build version 0.8

--- Assets/scripts/PlayerControls.cs
+++ Assets/scripts/PlayerControls.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField]float xControl = 20f;
     [SerializeField]float yControl = 20f;
     // Start is called before the first frame update
     [SerializeField] float xRange = 7f;
-    [SerializeField] float yRange =5f;
+    [SerializeField] float yRange =10f;
+
+    [SerializeField] float positionPitchFactor = -20f;
+    [SerializeField] float controlPitchFactor = -40f;
+    float xThrow, yThrow;
     void Start()
     {
         
@@ -17,23 +21,39 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.timeSinceLevelLoad > 20.0) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+        
+
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToLocalPosition = transform.localPosition.y * positionPitchFactor ;
+        float pitchDueToControlThrow = yThrow*controlPitchFactor;
+        float pitch = pitchDueToLocalPosition + pitchDueToControlThrow;
+        float yaw =0;
+        float roll= 0;
+
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         // Debug.Log("Vertical = " + yThrow);
         // Debug.Log("Horizontal = " + xThrow); 
         float xOffset = xThrow * Time.deltaTime * xControl;
         float yOffset = yThrow * Time.deltaTime * yControl;
         float rawXpos = transform.localPosition.x + xOffset;
-        float rawYpos = transform.localPosition.y +yOffset;
+        float rawYpos = transform.localPosition.y + yOffset;
 
-        float xClampedPos = Mathf.Clamp(rawXpos,-xRange,xRange);
-         float yClampedPos = Mathf.Clamp(rawYpos,-yRange,yRange);
+        float xClampedPos = Mathf.Clamp(rawXpos, -xRange, xRange);
+        float yClampedPos = Mathf.Clamp(rawYpos, -yRange, yRange);
         transform.localPosition = new Vector3(xClampedPos,
                                           yClampedPos,
                                           transform.localPosition.z);
-
-        
     }
 }
